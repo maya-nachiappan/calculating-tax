132286fcab25c0e01b4c4855f0639797010661bf
maya-nachiappan <maya.n102304@gmail.com>
2024-09-06 23:20:58 -0700

    Initial Commit

---
+++ assignment 2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  assignment 2
+//
+//  Created by maya nachiappan on 9/6/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
