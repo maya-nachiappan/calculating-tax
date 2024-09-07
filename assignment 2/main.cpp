8a5638da146d364bd2ec1dac17698f1f2be01ba1
maya-nachiappan <maya.n102304@gmail.com>
2024-09-06 23:31:05 -0700

    declaring variables

--- assignment 2/main.cpp
+++ assignment 2/main.cpp
@@ -1,14 +1,14 @@
-//
-//  main.cpp
 //  assignment 2
 //
 //  Created by maya nachiappan on 9/6/24.
 //
+// assignment to show the sales tax, estate tax, county tax and total tax needed to be paid on an income of 95,000.
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main ()
+{
+    int income, estateTax, countyTax, totalTax;
 }
+
